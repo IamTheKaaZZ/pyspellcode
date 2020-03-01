3fdce359c505af284b155c1ce088fb9f78274474
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2020-03-01 16:23:04 -0600

    Add small corpus to test on.

---
+++ example/example1.cpp
@@ -0,0 +1,6 @@
+/* First line of main doxygen in example1
+ * @returns sometypoinreturn
+ */
+int main() {
+  return 0; // sometypoinacomment
+}
---
+++ example/example2.cpp
@@ -0,0 +1,9 @@
+/* First line of main doxygen in example2
+ * @throws someexception typointhrows
+ * @returns 0
+ */
+int main() {
+  return 0;
+}
+
+// sometypo in comment after main
