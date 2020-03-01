2a0e0c5a512f5f450d394d81a52fc44423e8c236
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2020-03-01 16:23:04 -0600

    Add small corpus to test on.

---
+++ example/example2.cpp
@@ -0,0 +1,9 @@
+/* First line of main doxygen in example2.
+ * @throws someException typoInDoxygenThrows
+ * @returns 0
+ */
+int main() {
+  return 0;
+}
+
+// orange typoInCommentAtOutermostScope apple
