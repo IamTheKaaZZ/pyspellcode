c448faf4574bb5ac4bba43d38960a7edf181472a
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2020-03-02 09:58:40 -0600

    Skip rev/fun/inverse.

--- spell_check_math.sh
+++ spell_check_math.sh
@@ -2,7 +2,9 @@
 
 cd ..
 
+# Something about rev/fun/inverse is causing all of Eigen to be pulled in and
+# spellchecked. The other files that include Eigen somehow don't do that.
 
-export files=$(find math/stan/math/prim/meta -type f \( -name "*.hpp" -o -name "*.cpp" \))
+export files=$(find math/stan/math/fwd/ -type f \( \( -name "*.hpp" -o -name "*.cpp" \) -not \( -wholename "*rev/fun/inverse.hpp" \) \) )
 
 pyspellcode/spell-check.py -std=c++14 --collect --show-file-progress -fparse-all-comments --personal-dict=pyspellcode/dictionary --extra-clang-arg='-D_REENTRANT' --include-dir=math/. --include-dir=math/lib/eigen_3.3.3 --include-dir=math/lib/boost_1.72.0 --include-dir=math/lib/tbb_2019_U8/include --include-dir=math/lib/sundials_5.1.0/include --include-dir=math/lib/gtest_1.8.1 $files
